927ad58c6419b2d36fb9815a3d752dd3003ec6a4
jaredthecarrot <jaredmccain.jam@gmail.com>
2024-05-06 14:24:19 -0500

    init

---
+++ calculator_backend.py
@@ -0,0 +1,11 @@
+class Calculator:
+    def __init__():
+        pass
+    def addition(x, y):
+        pass
+    def subtraction(x, y):
+        pass
+    def multiplication(x, y):
+        pass
+    def division(x,y):
+        pass
