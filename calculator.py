7c0279dbb3170084aa5ff5fa725985c6dcf6802b
jaredthecarrot <jaredmccain.jam@gmail.com>
2024-08-28 22:21:45 -0500

    easy cli calc

--- calculator.py
+++ calculator.py
@@ -1,27 +1,34 @@
 class Calculator:
     def operation(op1, operator, op2):
         match operator:
-            case "+":
+            case "1":
                 return op1 + op2
-            case "-":
+            case "2":
                 return op1 - op2
-            case "*":
+            case "3":
                 return op1 * op2
-            case "/":
+            case "4":
                 try:
                     result = op1 / op2
                     return result
                 except ZeroDivisionError:
                     return 'ERROR'
-            case "^":
+            case "5":
                 return op1 ** op2
-            case "^^":
+            case "6":
                 return op1 ** (1/op2)
             case _:
                 print("INVALID")
 
 calc = Calculator
 
-result = calc.operation(8, "^^", 3)
+print('Enter the number of the operation you wish to perform.\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Square\n6.Inverse Square\n')
 
-print(result)
+while True:
+    oper = input()
+    try:
+        op1 = float(input('Enter first operand: '))
+        op2 = float(input('Enter second operand: '))
+        print(calc.operation(op1, oper, op2))
+    except TypeError:
+        print('Invalid type.')
